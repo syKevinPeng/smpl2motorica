fix the rotation in smpl2keypoint.py

dataloader.py
@@ -27,6 +27,7 @@ from smpl2keypoint import (
     motorica2smpl,
     load_dummy_motorica_data,
     get_motorica_skeleton_names,
+    motorica_to_smpl_mapping
 )
 from keypoint_fk import ForwardKinematics
 
@@ -34,8 +35,8 @@ from keypoint_fk import ForwardKinematics
 class AlignmentDataset(Dataset):
     def __init__(self, data_dir, segment_length=50, force_reprocess=False, mode = "train"):
         self.mode = mode
-        if mode not in ["train", "val", "predict"]:
-            raise ValueError("mode should be one of ['train', 'val', 'predict']")
+        if mode not in ["train", "validate", "predict"]:
+            raise ValueError("mode should be one of ['train', 'validate', 'predict']")
         self.data_dir = Path(data_dir)
         self.smpl_files = sorted(list(self.data_dir.glob("*_smpl.pkl")))
         self.keypoint_files = sorted(list(self.data_dir.glob("*_motorica.pkl")))
@@ -51,8 +52,9 @@ class AlignmentDataset(Dataset):
             print("Preprocessing data")
             self.all_data = self.preprocess_data()
             self.save_all_data(self.processed_file_save_path)
-        if self.mode == "val":
-            self.all_data = self.all_data.iloc[:100]
+        if self.mode == "validate":
+            # self.all_data = self.all_data.iloc[:100]
+            self.all_data = self.all_data
         if self.mode == "predict":
             self.all_data = self.keypoint_files
             self.get_longest_sequence_length()
@@ -61,7 +63,7 @@ class AlignmentDataset(Dataset):
         return len(self.all_data) // self.segment_length
 
     def __getitem__(self, idx):
-        if self.mode == "train" or self.mode == "val":
+        if self.mode == "train" or self.mode == "validate":
             if isinstance(idx, slice):
                 raise NotImplementedError("Slicing is not supported")
             else:
@@ -101,7 +103,7 @@ class AlignmentDataset(Dataset):
 
             # processing keypoint_df: transfer from dataframe to tensor
             keypoint_data = keypoint_df.values
-            keypoint_data = torch.tensor(keypoint_data.reshape(sequence_length, -1, 3), dtype=torch.float32)
+            keypoint_data = keypoint_data.reshape(sequence_length, -1, 3)
             keypoint_col = keypoint_df.columns
             return (keypoint_data, smpl_dict)
         
@@ -271,15 +273,15 @@ class AlignmentDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.mode = mode
-        if self.mode not in ["train", "val", "predict"]:
-            raise ValueError("mode should be one of ['train', 'val', 'predict']")
+        if self.mode not in ["train", "validate", "predict"]:
+            raise ValueError("mode should be one of ['train', 'validate', 'predict']")
         self.setup()
 
     def setup(self, stage=None):
         if self.mode == "train":
             self.train_dataset = AlignmentDataset(self.data_dir)
-        elif self.mode == "val":
-            self.val_dataset = AlignmentDataset(self.data_dir, mode="val")
+        elif self.mode == "validate":
+            self.val_dataset = AlignmentDataset(self.data_dir, mode="validate")
         elif self.mode == "predict":
             self.predict_dataset = AlignmentDataset(self.data_dir, mode="predict")
         
@@ -374,39 +376,109 @@ def main():
     #     keypoint_batch, keypoint_col_name, smpl_batch = alignment_data[i]
 
     # # For visualization and testing
-    for keypoint_batch, smpl_batch in alighment_dataset:
-        print(keypoint_batch.shape)
-        print(smpl_batch["smpl_body_pose"].shape)
+    for keypoint_data, smpl_batch in alighment_dataset:
         break
 
 
+    keypoint_fk = ForwardKinematics()
 
-
+    batch_size, len_of_sequence, _ = smpl_batch["smpl_body_pose"].shape
     # # convert from matrix to euler angles
     # # keypoint_batch = conti_angle_rep.matrix_to_euler_angles(keypoint_batch, convention="XYZ")
     # # reshape back to (num_frames, num_keypoints x 3)
     # keypoint_batch = keypoint_batch.reshape(keypoint_batch.shape[0], -1)
 
     # # processing SMPL data
-    # pose = smpl_batch["smpl_body_pose"]
-    # transl = smpl_batch["smpl_transl"]
-    # global_orient = smpl_batch["smpl_global_orient"]
+    pose = smpl_batch["smpl_body_pose"].reshape(-1, 69)
+    transl = smpl_batch["smpl_transl"].reshape(-1, 3)
+    global_orient = smpl_batch["smpl_global_orient"].reshape(-1, 3)
 
-    # smpl_model = smplx.create(
-    #     model_path=smpl_model_path,
-    #     model_type="smpl",
-    #     return_verts=True,
-    #     batch_size=len(pose),
-    # )
-    # smpl_output = smpl_model(
-    #     global_orient=torch.tensor(global_orient, dtype=torch.float32),
-    #     body_pose=torch.tensor(pose, dtype=torch.float32),
-    #     transl=torch.tensor(transl, dtype=torch.float32),
-    # )
-    # smpl_joints_loc = smpl_output.joints.detach().cpu().numpy().squeeze()
-    # smpl_vertices = smpl_output.vertices.detach().cpu().numpy().squeeze()
-    # smpl_joints_loc = smpl_joints_loc[:, :24, :]
+    smpl_model = smplx.create(
+        model_path=smpl_model_path,
+        model_type="smpl",
+        return_verts=True,
+        batch_size=len(pose),
+    )
+    smpl_output = smpl_model(
+        global_orient=torch.tensor(global_orient, dtype=torch.float32),
+        body_pose=torch.tensor(pose, dtype=torch.float32),
+        transl=torch.tensor(transl, dtype=torch.float32),
+    )
+    smpl_joints_loc = smpl_output.joints.detach().cpu().numpy().squeeze()
+    smpl_vertices = smpl_output.vertices.detach().cpu().numpy().squeeze()
+    smpl_joints_loc = smpl_joints_loc[:, :24, :]
+    smpl_joint_names = get_SMPL_skeleton_names()
+    smpl_joints_loc = smpl_joints_loc[:, [smpl_joint_names.index(joint) for joint in motorica_to_smpl_mapping().values()],:]
+        # swap from XYZ to ZXY
+    smpl_joints_loc = smpl_joints_loc[:, :, [2, 0, 1]]
 
+    batch_size, len_of_sequence, num_joints = keypoint_data.shape[:3]
+    keypoint_data = keypoint_data.view(batch_size*len_of_sequence, num_joints, 3)
+    keypoint_position = keypoint_fk.forward(keypoint_data.reshape(-1, 60))
+    keypoint_position = keypoint_position.reshape(batch_size, len_of_sequence, -1, 3)
+    keypoint_rot_loc = keypoint_position.view(batch_size, -1, 19, 3)
+
+    def visualize_keypoint_data(ax, frame: int, df: pd.DataFrame, skeleton = None):
+        if skeleton is None:
+            skeleton = keypoint_fk.get_skeleton()
+        joint_names = get_motorica_skeleton_names()
+        for idx, joint in enumerate(joint_names):
+            # ^ In mocaps, Y is the up-right axis
+            parent_x = df[f"{joint}_Xposition"].iloc[frame]
+            parent_y = df[f"{joint}_Zposition"].iloc[frame]
+            parent_z = df[f"{joint}_Yposition"].iloc[frame]
+            # print(f'joint: {joint}: parent_x: {parent_x}, parent_y: {parent_y}, parent_z: {parent_z}')
+            ax.scatter(xs=parent_x, ys=parent_y, zs=parent_z, alpha=0.6, c="b", marker="o")
+
+            children_to_draw = [
+                c for c in skeleton[joint]["children"] if c in joint_names
+            ]
+
+            for c in children_to_draw:
+                # ^ In mocaps, Y is the up-right axis
+                child_x = df[f"{c}_Xposition"].iloc[frame]
+                child_y = df[f"{c}_Zposition"].iloc[frame]
+                child_z = df[f"{c}_Yposition"].iloc[frame]
+                
+                ax.plot(
+                    [parent_x, child_x],
+                    [parent_y, child_y],
+                    [parent_z, child_z],
+                    # "k-",
+                    lw=4,
+                    c="black",
+                )
+
+            ax.text(
+                x=parent_x - 0.01,
+                y=parent_y - 0.01,
+                z=parent_z - 0.01,
+                s=f"{idx}:{joint}",
+                fontsize=5,
+            )
+        
+
+        return ax
+    adjusted_keypoint_loc_df = keypoint_fk.convert_to_dataframe(keypoint_rot_loc.reshape(-1, 19, 3))
+    fig = plt.figure(figsize=(20, 10))
+    input_loc_ax = fig.add_subplot(121, projection="3d")
+    input_loc_ax = visualize_keypoint_data(input_loc_ax, 0, adjusted_keypoint_loc_df)
+    input_loc_ax.set_title("Adjusted Keypoint")
+    smpl_loc_df = keypoint_fk.convert_to_dataframe(positions=torch.tensor(smpl_joints_loc.reshape(-1, 19, 3)))
+    smpl_loc_ax = fig.add_subplot(122, projection="3d")
+    smpl_loc_ax = visualize_keypoint_data(smpl_loc_ax, 0, smpl_loc_df)
+    smpl_loc_ax.set_title("SMPL Model")
+    # set xyz axis
+    smpl_loc_ax.set_xlabel("X")
+    smpl_loc_ax.set_ylabel("Y")
+    smpl_loc_ax.set_zlabel("Z")
+    
+    plt.savefig("dataloader_testing_fig.png")
+
+
+
+
+    
     # # processing motorica data
     # keypoint_col_name = selected_col = [
     #         "Hips_Xposition",
@@ -419,7 +491,7 @@ def main():
     # keypoint_pos = keypoint_fk.forward(keypoint_batch)
     # position_df = keypoint_fk.convert_to_dataframe(keypoint_pos)
 
-
+    
 
     # motorica_dummy_data = load_dummy_motorica_data()
     # # motorica_dummy_data.values = keypoint_batch_df

keypoint_fk.py
@@ -8,7 +8,7 @@ import sys
 import matplotlib.pyplot as plt
 
 sys.path.append("../")
-from typing import Union
+from smpl2motorica.utils.keypoint_skeleton import get_keypoint_skeleton
 from smpl2motorica.utils.bvh import BVHParser
 from smpl2motorica.utils.pymo.preprocessing import MocapParameterizer
 from smpl2motorica.smpl2keypoint import (
@@ -23,7 +23,7 @@ from smpl2motorica.smpl2keypoint import (
 
 class ForwardKinematics:
     def __init__(self):
-        self.skeleton = self.get_skeleton()
+        self.skeleton = get_keypoint_skeleton()
         self.joints = self.skeleton.keys()
         (
             self.joint_names,
@@ -63,166 +63,7 @@ class ForwardKinematics:
         my_skeleton = skeleton_scaler(my_skeleton, ratio)
         return my_skeleton
 
-    def get_skeleton(self):
-        return {
-            "Hips": {
-                "parent": None,
-                "channels": [
-                    "Xposition",
-                    "Yposition",
-                    "Zposition",
-                    "Zrotation",
-                    "Xrotation",
-                    "Yrotation",
-                ],
-                "offsets": np.array([-0.204624, 0.864926, -0.962418]),
-                "order": "ZXY",
-                "children": ["Spine", "LeftUpLeg", "RightUpLeg"],
-            },
-            "Spine": {
-                "parent": "Hips",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.0, 0.0777975, 0.0]),
-                "order": "ZXY",
-                "children": ["Spine1"],
-            },
-            "Spine1": {
-                "parent": "Spine",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-1.57670e-07, 2.26566e-01, 7.36298e-07]),
-                "order": "ZXY",
-                "children": ["Neck", "LeftShoulder", "RightShoulder"],
-            },
-            "Neck": {
-                "parent": "Spine1",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.0, 0.249469, 0.0]),
-                "order": "ZXY",
-                "children": ["Head"],
-            },
-            "Head": {
-                "parent": "Neck",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.0, 0.147056, 0.018975]),
-                "order": "ZXY",
-                "children": [],
-                # "children": ["Head_Nub"],
-            },
-            "LeftShoulder": {
-                "parent": "Spine1",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.037925, 0.208193, -0.0005065]),
-                "order": "ZXY",
-                "children": ["LeftArm"],
-            },
-            "LeftArm": {
-                "parent": "LeftShoulder",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([1.24818e-01, -1.24636e-07, 0.00000e00]),
-                "order": "ZXY",
-                "children": ["LeftForeArm"],
-            },
-            "LeftForeArm": {
-                "parent": "LeftArm",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([2.87140e-01, 1.34650e-07, 6.52025e-06]),
-                "order": "ZXY",
-                "children": ["LeftHand"],
-            },
-            "LeftHand": {
-                "parent": "LeftForeArm",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.234148, 0.00116565, 0.00321146]),
-                "order": "ZXY",
-                "children": [],
-                # "children": [
-                #     "LeftHandThumb1",
-                #     "LeftHandIndex1",
-                #     "LeftHandMiddle1",
-                #     "LeftHandRing1",
-                #     "LeftHandPinky1",
-                # ],
-            },
-            "RightShoulder": {
-                "parent": "Spine1",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-0.0379391, 0.208193, -0.00050652]),
-                "order": "ZXY",
-                "children": ["RightArm"],
-            },
-            "RightArm": {
-                "parent": "RightShoulder",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-0.124818, 0.0, 0.0]),
-                "order": "ZXY",
-                "children": ["RightForeArm"],
-            },
-            "RightForeArm": {
-                "parent": "RightArm",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-2.87140e-01, -3.94596e-07, 1.22370e-06]),
-                "order": "ZXY",
-                "children": ["RightHand"],
-            },
-            "RightHand": {
-                "parent": "RightForeArm",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-0.237607, 0.00081803, 0.00144663]),
-                "order": "ZXY",
-                "children": [],
-                # "children": [
-                #     "RightHandThumb1",
-                #     "RightHandIndex1",
-                #     "RightHandMiddle1",
-                #     "RightHandRing1",
-                #     "RightHandPinky1",
-                # ],
-            },
-            "LeftUpLeg": {
-                "parent": "Hips",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.0948751, 0.0, 0.0]),
-                "order": "ZXY",
-                "children": ["LeftLeg"],
-            },
-            "LeftLeg": {
-                "parent": "LeftUpLeg",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([2.47622e-07, -3.57160e-01, -1.88071e-06]),
-                "order": "ZXY",
-                "children": ["LeftFoot"],
-            },
-            "LeftFoot": {
-                "parent": "LeftLeg",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.00057702, -0.408583, 0.00046285]),
-                "order": "ZXY",
-                "children": [],
-                # "children": ["LeftToeBase"],
-            },
-            "RightUpLeg": {
-                "parent": "Hips",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-0.0948751, 0.0, 0.0]),
-                "order": "ZXY",
-                "children": ["RightLeg"],
-            },
-            "RightLeg": {
-                "parent": "RightUpLeg",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([-2.56302e-07, -3.57160e-01, -2.17293e-06]),
-                "order": "ZXY",
-                "children": ["RightFoot"],
-            },
-            "RightFoot": {
-                "parent": "RightLeg",
-                "channels": ["Zrotation", "Xrotation", "Yrotation"],
-                "offsets": np.array([0.00278006, -0.403849, 0.00049768]),
-                "order": "ZXY",
-                "children": [],
-                # "children": ["RightToeBase"],
-            },
-        }
+
 
     def _parse_skeleton(self, skeleton):
         # Find root joint
@@ -418,7 +259,7 @@ if __name__ == "__main__":
     # fk.grad_check()
 
     mocap_track = load_dummy_motorica_data()
-    mocap_track.skeleton = fk.get_skeleton()
+    mocap_track.skeleton = get_keypoint_skeleton()
     mocap_df = mocap_track.values
 
     # force the starting position to be zero all the time

train.py
@@ -20,9 +20,10 @@ from smpl2motorica.utils import conti_angle_rep
 from smpl2motorica.smpl2keypoint import (
     load_dummy_motorica_data,
     get_SMPL_skeleton_names,
+    motorica_to_smpl_mapping,
     expand_skeleton,
     get_motorica_skeleton_names,
-    smpl_motorica_mapping,
+    smpl_to_motorica_mapping,
     SMPL_visulize_a_frame,
     motorica_draw_stickfigure3d
 )
@@ -112,9 +113,13 @@ class KeypointModel(pl.LightningModule):
             return smpl_joints_loc, smpl_verts
         # only select the joint we needed
         smpl_joint_names = get_SMPL_skeleton_names()
-        selected_joints = list(smpl_motorica_mapping().keys())
+        # smpl_df = pd.DataFrame(smpl_joints_loc.reshap(smpl_joints_loc.shape[0], -1), columns=expand_skeleton(smpl_joint_names))
+        # print(f'smpl_df')
+        # convert from keypoint to smpl joint
         # if smpl_joint_name is in selected_joints, then keep the joint
-        smpl_joints_loc = smpl_joints_loc[:, [smpl_joint_names.index(joint) for joint in selected_joints],:].reshape(-1, len(selected_joints)*3)
+        smpl_joints_loc = smpl_joints_loc[:, [smpl_joint_names.index(joint) for joint in motorica_to_smpl_mapping().values()],:]
+        # swap from XYZ to ZXY
+        smpl_joints_loc = smpl_joints_loc[:, :, [2, 0, 1]]
         return smpl_joints_loc
 
     def keypoint_to_rot_mat(self, keypoint_data: torch.tensor):
@@ -189,13 +194,16 @@ class KeypointModel(pl.LightningModule):
         return keypoint_euler
 
     def keypoint_forward_kinematics(self, keypoint_data):
+
         # keypoint data contains the translation and rotation of the keypoints
         # first 3 columns are the translation, and the rest are the rotation that have the same order as motorica2smpl
         # merge the batch dimension with the len_of_sequence dimension
         # input size: (batch_size, len_of_sequence, 20, 3)
+        # output size: (batch_size, len_of_sequence, 20, 3)
         batch_size, len_of_sequence, num_joints = keypoint_data.shape[:3]
         keypoint_data = keypoint_data.view(batch_size*len_of_sequence, num_joints, 3)
         keypoint_position = self.keypoint_fk.forward(keypoint_data.reshape(-1, 60))
+        keypoint_position = keypoint_position.reshape(batch_size, len_of_sequence, -1, 3)
         return keypoint_position
 
     def smpl_dict_to_cuda(self, smpl_dict):
@@ -221,17 +229,11 @@ class KeypointModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         print("Performing Validation Step")
         print(f'WARNING: FOR DEBUGGING ONLY; MODEL ONLY RETURN IDENTITY MATRIX')
-        #for testing only
         keypoint_rot_euler, smpl_batch = batch
-        # check if euler is in radian
-        # assert torch.max(keypoint_rot_euler) <= 3.14, "keypoint rotation is not in radian"
-        # keypoint_rot_euler = torch.rand_like(keypoint_rot_euler)
+        batch_size, len_of_sequence, _, _ = keypoint_rot_euler.shape
         keypoint_rot_mat = self.keypoint_to_rot_mat(keypoint_rot_euler).to(
             self.device
         ) #(batch_size, seq_length, 1+19, 9)
-
-        # converted_back = self.rot_mat_to_keypoint(keypoint_rot_mat)
-        # assert torch.allclose(keypoint_rot_euler, converted_back, atol=1e-4), "keypoint rotation and converted back keypoint rotation are not close enough"
 
         smpl_batch = self.smpl_dict_to_cuda(smpl_batch)
         smpl_joint_loc, smpl_vertices = self.smpl_forward_kinematics(smpl_batch, return_verts=True)
@@ -242,22 +244,35 @@ class KeypointModel(pl.LightningModule):
         predicted_rot_transl[:, :, 1:, :] = torch.eye(3).view(1, 1, 1, 9).repeat(
             predicted_rot_transl.shape[0],predicted_rot_transl.shape[1], 19, 1
         )
-        adjusted_keypoint = self.apply_prediction_to_keypoint(
-            predicted_rot_transl, keypoint_rot_mat
-        ) # (batch_size, len_of_sequence, 20, 3)
-        keypoint_rot_loc = self.keypoint_forward_kinematics(keypoint_rot_euler)
-        adjusted_keypoint_loc = self.keypoint_forward_kinematics(adjusted_keypoint)
+        # adjusted_keypoint = self.apply_prediction_to_keypoint(
+        #     predicted_rot_transl, keypoint_rot_mat
+        # ) # (batch_size, len_of_sequence, 20, 3)
+        # keypoint_rot_loc = self.keypoint_forward_kinematics(keypoint_rot_euler)
+        # adjusted_keypoint_loc = self.keypoint_forward_kinematics(adjusted_keypoint)
+
+        loss = self.loss(
+            predicted_rot_transl, keypoint_rot_mat, smpl_batch, reg_weight=0
+        )
 
         # keypoint_rot_euler should have the same value adjusted_keypoint_loc. Check their values
         # print("keypoint_rot_euler", keypoint_rot_euler[0][0])
         # print("adjusted_keypoint", adjusted_keypoint[0][0])
         # assert torch.allclose(keypoint_rot_euler, adjusted_keypoint, atol=1e-6), "SMPL joint location and adjusted keypoint location are not close enough"
-        self.prediction_visualization(
-            smpl_loc = smpl_joint_loc, 
-            smpl_vertices = smpl_vertices, 
-            keypoint_loc=keypoint_rot_loc, 
-            predicted_loc=adjusted_keypoint_loc, 
-            output_path="validate_keypoint.png")
+        # self.prediction_visualization(
+        #     smpl_loc = smpl_joint_loc, 
+        #     smpl_vertices = smpl_vertices, 
+        #     keypoint_loc=keypoint_rot_loc, 
+        #     predicted_loc=adjusted_keypoint_loc, 
+        #     output_path="validate_keypoint.png")
+        # adjusted_keypoint_loc = adjusted_keypoint_loc.reshape(batch_size*len_of_sequence,-1)
+        # smpl_joint_loc
+        # print(f'adjusted_keypoint_loc shape: {adjusted_keypoint_loc.shape}')
+        # print(f'smpl_joint_loc shape: {smpl_joint_loc.shape}')
+        # # calculate the MPJP loss on uncalibrated poses
+        # loss = self.mse_loss(adjusted_keypoint_loc, smpl_joint_loc)
+        self.log('val_loss', loss)
+        self.log("epoch_val_loss", loss, on_epoch=True)
+        
         
     def predict_step(self, batch, batch_idx):
         prediction_output_dir = Path("/fs/nexus-projects/PhysicsFall/smpl2motorica/prediction_result")
@@ -453,7 +468,7 @@ class KeypointModel(pl.LightningModule):
         return predicted_keypoint
 
     def loss(
-        self, predicted_rot_transl, keypoint_batch, smpl_batch
+        self, predicted_rot_transl, keypoint_batch, smpl_batch, reg_weight=1
     ):
         batch_size = keypoint_batch.shape[0]
         len_of_sequence = keypoint_batch.shape[1]
@@ -462,23 +477,46 @@ class KeypointModel(pl.LightningModule):
         ) # (batch_size, len_of_sequence, 20, 3)
         adjusted_keypoint_loc = self.keypoint_forward_kinematics(adjusted_keypoint)
         # apply forward kinematics to the smpl data
-        smpl_loc = self.smpl_forward_kinematics(smpl_batch)
+        smpl_loc = self.smpl_forward_kinematics(smpl_batch).reshape(batch_size, -1, 19, 3)
+        
         # calculate the loss
-        loss = self.mse_loss(adjusted_keypoint_loc.reshape(batch_size*len_of_sequence,-1), smpl_loc)
-        return loss 
+        mpjpe_loss = self.mse_loss(adjusted_keypoint_loc, smpl_loc)
+
+        # regularization term
+        reg_weight = 1
+        rot_mat = predicted_rot_transl[:, :, 1:, :].view(batch_size, len_of_sequence, 19, 3, 3)
+        identity_mat = torch.eye(3).view(1, 1, 1, 3, 3).repeat(
+            batch_size, len_of_sequence, 19, 1, 1
+        ).to(self.device)
+        reg_loss = reg_weight * torch.norm(rot_mat - identity_mat, p="fro", dim=(-2, -1)).mean()
+
+        # for debugging only visualize two skeletons
+        adjusted_keypoint_loc_df = self.keypoint_fk.convert_to_dataframe(adjusted_keypoint_loc.reshape(-1, 19, 3))
+        fig = plt.figure(figsize=(20, 10))
+        input_loc_ax = fig.add_subplot(121, projection="3d")
+        input_loc_ax = self.visualize_keypoint_data(input_loc_ax, 0, adjusted_keypoint_loc_df)
+        input_loc_ax.set_title("Adjusted Keypoint")
+        smpl_loc_df = self.keypoint_fk.convert_to_dataframe(smpl_loc.reshape(-1, 19, 3))
+        smpl_loc_ax = fig.add_subplot(122, projection="3d")
+        smpl_loc_ax = self.visualize_keypoint_data(smpl_loc_ax, 0, smpl_loc_df)
+        smpl_loc_ax.set_title("SMPL Model")
+
+        fig.savefig("debugging_fig.png")
+        exit()
+        return mpjpe_loss + reg_loss
 
 
 def main():
-    mode = "predict"
+    # mode = "predict"
     ckpt = Path("/fs/nexus-projects/PhysicsFall/smpl2motorica/checkpoints/smpl2keypoint-epoch=04-train_loss=0.29.ckpt")
     if not ckpt.exists():
         raise FileNotFoundError(f"Checkpoint {ckpt} does not exist.")
-    # mode = "validate"
-    # mode = "predict"
+    mode = "validate"
+    # mode = "train"
 
 
     data_dir = "/fs/nexus-projects/PhysicsFall/smpl2motorica/data/alignment_dataset"
-    batch_size = 1
+    batch_size = 16
     num_workers = 4
 
 
@@ -486,17 +524,21 @@ def main():
 
     model = KeypointModel()
     wandb_logger = WandbLogger(project="SMPL2Keypoint",
-                                name="train_1",
+                                name="train_3_with_reg",
                                 mode = "disabled"
                                           )
+    saving_dir = Path("/fs/nexus-projects/PhysicsFall/smpl2motorica/checkpoints")
+    exp_id = wandb_logger.experiment.id
+    if not (saving_dir/exp_id).exists():
+        (saving_dir/exp_id).mkdir(parents=True)
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         monitor='train_loss',
-        dirpath='/fs/nexus-projects/PhysicsFall/smpl2motorica/checkpoints',
+        dirpath=str(saving_dir/exp_id),
         filename='smpl2keypoint-{epoch:02d}-{train_loss:.2f}',
         mode='min',
         every_n_epochs=5  # Save every 10 epochs
     )
-    trainer = Trainer(max_epochs=500, logger=wandb_logger, callbacks = [checkpoint_callback])
+    trainer = Trainer(max_epochs=300, logger=wandb_logger, callbacks = [checkpoint_callback])
     if mode == "train":
         trainer.fit(model, data_module.train_dataloader())
     elif mode == "validate":